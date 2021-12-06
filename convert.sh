066dcbecd2fa386b508697bfb03ef7d755ef2b0e
tmax818 <tylermaxwell661@gmail.com>
2021-12-06 13:18:25 -0800

    update

--- convert.sh
+++ convert.sh
@@ -2,3 +2,5 @@
 
 md-to-pdf --stylesheet "style.css" --document-title="resume" ./tylerMaxwell.md
 pandoc -f markdown -t html tylerMaxwell.md -o index.html --self-contained --css=style.css
+
+cp tylerMaxwell.md README.md
